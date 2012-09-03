b2592cb337f12bdde37129346090568206781a5d
Joshua Lamson <jlamson@mymail.mines.edu>
2012-09-03 11:53:23 -0600

    APT3 Extra Credit done. Added surplus radio buttons programmatically. NOT YET TESTED DUE TO LIBRARY ISSUE.

--- LunchList/src/csci498/jlamson/lunchlist/LunchList.java
+++ LunchList/src/csci498/jlamson/lunchlist/LunchList.java
@@ -1,10 +1,12 @@
 package csci498.jlamson.lunchlist;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class LunchList extends Activity {
@@ -15,11 +17,13 @@ public class LunchList extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunch_list);
         
+        addSurplusRadioButtons(savedInstanceState);
+        
         Button save = (Button)findViewById(R.id.save);
         save.setOnClickListener(onSave);
     }
-    
-    private View.OnClickListener onSave = new View.OnClickListener() {
+
+	private View.OnClickListener onSave = new View.OnClickListener() {
 		public void onClick(View v) {
 			EditText name = (EditText)findViewById(R.id.name);
 			EditText address = (EditText)findViewById(R.id.addr);
@@ -42,5 +46,14 @@ public class LunchList extends Activity {
 		}
 	};
 
-   
+	private void addSurplusRadioButtons(Context context) {
+		RadioGroup types = (RadioGroup)findViewById(R.id.types);
+		
+		int bigNumber = 20;
+		for(int i=0; i < bigNumber; i++) {
+			RadioButton rb = new RadioButton(context);
+			rb.setText("fooblah");
+			types.addView(rb);
+		}
+	}
 }
