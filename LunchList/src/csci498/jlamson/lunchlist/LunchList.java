bc65f51fb2ff63365b59d7e103dd81c196cdc4c5
Joshua Lamson <jlamson@mymail.mines.edu>
2012-09-03 11:46:31 -0600

    Added Radio group to layout, model, and to "onClick" for save.

--- LunchList/src/csci498/jlamson/lunchlist/LunchList.java
+++ LunchList/src/csci498/jlamson/lunchlist/LunchList.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class LunchList extends Activity {
 	Restaurant restaurant = new Restaurant();
@@ -22,9 +23,22 @@ public class LunchList extends Activity {
 		public void onClick(View v) {
 			EditText name = (EditText)findViewById(R.id.name);
 			EditText address = (EditText)findViewById(R.id.addr);
+			RadioGroup types = (RadioGroup)findViewById(R.id.types);
 			
 			restaurant.setName(name.getText().toString());
 			restaurant.setAddress(address.getText().toString());
+			
+			switch (types.getCheckedRadioButtonId()) {
+			case R.id.sit_down:
+				restaurant.setType("sit_down");
+				break;
+			case R.id.take_out:
+				restaurant.setType("take_out");
+				break;
+			case R.id.delivery:
+				restaurant.setType("delivery");
+				break;
+			}
 		}
 	};
 
