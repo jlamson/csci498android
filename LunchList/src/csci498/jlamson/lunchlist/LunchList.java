c1b8e09ec530eeddc06f1fcab255a502f7d77d12
Joshua Lamson <jlamson@mymail.mines.edu>
2012-09-03 11:14:37 -0600

    Built the Restaurant class and utilize in in LunchList.java (APT2)

--- LunchList/src/csci498/jlamson/lunchlist/LunchList.java
+++ LunchList/src/csci498/jlamson/lunchlist/LunchList.java
@@ -1,20 +1,32 @@
 package csci498.jlamson.lunchlist;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LunchList extends Activity {
-
+	Restaurant restaurant = new Restaurant();
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunch_list);
+        
+        Button save = (Button)findViewById(R.id.save);
+        save.setOnClickListener(onSave);
     }
+    
+    private View.OnClickListener onSave = new View.OnClickListener() {
+		public void onClick(View v) {
+			EditText name = (EditText)findViewById(R.id.name);
+			EditText address = (EditText)findViewById(R.id.addr);
+			
+			restaurant.setName(name.getText().toString());
+			restaurant.setAddress(address.getText().toString());
+		}
+	};
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_lunch_list, menu);
-        return true;
-    }
+   
 }
