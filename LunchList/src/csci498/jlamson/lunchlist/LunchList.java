842a75b1ec598cef2d6598eb5c4c529c5f72550d
Joshua Lamson <jlamson@mymail.mines.edu>
2012-09-27 02:56:07 -0600

    Completed first ec. Handler object go into a realm of threading i've never dealt with.

--- LunchList/src/csci498/jlamson/lunchlist/LunchList.java
+++ LunchList/src/csci498/jlamson/lunchlist/LunchList.java
@@ -85,6 +85,8 @@ public class LunchList extends TabActivity {
     		public void run() {
     			progress += incr;
     			setProgress(progress);
+    			((TextView)findViewById(R.id.thread_title)).setText("Thread is...");
+    			((TextView)findViewById(R.id.thread_status)).setText("working...");
     		}
     	});
     	
@@ -93,6 +95,7 @@ public class LunchList extends TabActivity {
     
     private Runnable longTask = new Runnable() {
     	public void run() {
+    		((TextView)findViewById(R.id.thread_title)).setText("Thread is...");
     		for(int i=0; i<20; i++) {
     			doSomeLongWork(500);
     		}
@@ -100,6 +103,7 @@ public class LunchList extends TabActivity {
 	    	runOnUiThread( new Runnable() {
 	    		public void run() {
 	    			setProgressBarVisibility(false);
+	    			((TextView)findViewById(R.id.thread_status)).setText("Done");
 	    		}
 	    	});
     	}
