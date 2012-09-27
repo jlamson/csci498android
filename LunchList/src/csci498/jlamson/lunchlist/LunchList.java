329ab5d92ba85532490a1825f5b14c246e38aaa5
Joshua Lamson <jlamson@mymail.mines.edu>
2012-09-27 01:13:02 -0600

    apt7: added menu, and resulting toast. Wrapped in a scroll view. Also changed the target to get the legacy menu button on the GN7

--- LunchList/src/csci498/jlamson/lunchlist/LunchList.java
+++ LunchList/src/csci498/jlamson/lunchlist/LunchList.java
@@ -7,6 +7,9 @@ import android.app.TabActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -19,6 +22,7 @@ import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LunchList extends TabActivity {
 	List<Restaurant> restaurants = new ArrayList<Restaurant>();
@@ -31,6 +35,8 @@ public class LunchList extends TabActivity {
     EditText address = null;
     RadioGroup types = null;
     EditText notes = null;
+    
+    Restaurant current = null;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -69,7 +75,29 @@ public class LunchList extends TabActivity {
         getTabHost().setCurrentTab(0);
     }
     
-	class RestaurantAdapter extends ArrayAdapter<Restaurant> {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	new MenuInflater(this).inflate(R.menu.option, menu);
+    	return(super.onCreateOptionsMenu(menu));
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	if(item.getItemId() == R.id.toast) {
+    		String message = "No Restaurant Selected";
+    		
+    		if(current != null) {
+    			message = current.getNotes();
+    		}
+    		
+    		Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    		
+    		return true;
+    	}
+    	return super.onOptionsItemSelected(item);
+    }
+
+    class RestaurantAdapter extends ArrayAdapter<Restaurant> {
 		public RestaurantAdapter() {
 			super(	LunchList.this,
 					android.R.layout.simple_list_item_1,
@@ -126,15 +154,15 @@ public class LunchList extends TabActivity {
 									View view,
 									int position,
 									long id) {
-			Restaurant r = restaurants.get(position);
+			current = restaurants.get(position);
 			
-			name.setText(r.getName());
-			address.setText(r.getAddress());
-			notes.setText(r.getNotes());
+			name.setText(current.getName());
+			address.setText(current.getAddress());
+			notes.setText(current.getNotes());
 			
-			if(r.getType().equals("sit_down")) {
+			if(current.getType().equals("sit_down")) {
 				types.check(R.id.sit_down);
-			} else if(r.getType().equals("take_out")) {
+			} else if(current.getType().equals("take_out")) {
 				types.check(R.id.take_out);
 			} else {
 				types.check(R.id.delivery);
@@ -146,30 +174,30 @@ public class LunchList extends TabActivity {
 	
 	private View.OnClickListener onSave = new View.OnClickListener() {
 		public void onClick(View v) {
-			Restaurant restaurant = new Restaurant();
+			current = new Restaurant();
 			
 			name = (EditText)findViewById(R.id.name);
 			address = (EditText)findViewById(R.id.addr);
 			types = (RadioGroup)findViewById(R.id.types);
 			notes = (EditText)findViewById(R.id.notes);
 			
-			restaurant.setName(name.getText().toString());
-			restaurant.setAddress(address.getText().toString());
-			restaurant.setNotes(notes.getText().toString());
+			current.setName(name.getText().toString());
+			current.setAddress(address.getText().toString());
+			current.setNotes(notes.getText().toString());
 			
 			switch (types.getCheckedRadioButtonId()) {
 			case R.id.sit_down:
-				restaurant.setType("sit_down");
+				current.setType("sit_down");
 				break;
 			case R.id.take_out:
-				restaurant.setType("take_out");
+				current.setType("take_out");
 				break;
 			case R.id.delivery:
-				restaurant.setType("delivery");
+				current.setType("delivery");
 				break;
 			}
 			
-			restaurantAdapter.add(restaurant);
+			restaurantAdapter.add(current);
 			addressAdapter.add(address.getText().toString());
 		}
 	};
