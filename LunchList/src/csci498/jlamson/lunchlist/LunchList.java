50ce2aff768b4fb511e7e19495344abf940e5a86
Joshua Lamson <jlamson@mymail.mines.edu>
2012-10-11 22:32:26 -0600

    condensed imports

--- LunchList/src/csci498/jlamson/lunchlist/LunchList.java
+++ LunchList/src/csci498/jlamson/lunchlist/LunchList.java
@@ -6,19 +6,8 @@ import java.util.List;
 import android.app.TabActivity;
 import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.RadioGroup;
-import android.widget.TabHost;
-import android.widget.TextView;
+import android.view.*;
+import android.widget.*;
 
 public class LunchList extends TabActivity {
 	List<Restaurant> restaurants = new ArrayList<Restaurant>();
