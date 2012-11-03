862e6406c8abcf35de7a5766d6983815d077ed25
Joshua Lamson <jlamson@mymail.mines.edu>
2012-11-03 16:29:05 -0600

    Added action to the RSS menu item. Tests if an internet connection exists, and ifso, starts FeedActivity (<-- not made yet)

--- LunchList/src/csci498/jlamson/lunchlist/DetailForm.java
+++ LunchList/src/csci498/jlamson/lunchlist/DetailForm.java
@@ -1,13 +1,18 @@
 package csci498.jlamson.lunchlist;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class DetailForm extends Activity {
 	
@@ -80,6 +85,34 @@ public class DetailForm extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.detail_option, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	
+    	if (item.getItemId() == R.id.feed) {
+    		
+    		if (isNetworkAvailable()) {
+    			Intent i = new Intent(this, FeedActivity.class);
+    			i.putExtra(FeedActivity.FEED_URL, feed.getText().toString());
+    			startActivity(i);
+    		} else {
+    			Toast
+					.makeText(this, "Sorry, no Internet connection is available", Toast.LENGTH_LONG)
+					.show();
+    		}
+    		
+    		return true;
+    	}
+    	
+    	return super.onOptionsItemSelected(item);
+    	
+    }
+    
+    private boolean isNetworkAvailable() {
+    	ConnectivityManager cm = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+    	NetworkInfo info = cm.getActiveNetworkInfo();
+    	return (info != null);
     }
     
     @Override
