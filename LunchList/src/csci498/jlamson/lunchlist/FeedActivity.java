c842e235a9673513a548891bef70a45a88af012d
Joshua Lamson <jlamson@mymail.mines.edu>
2012-11-03 17:05:15 -0600

    Created FeedTask which will make our http request. TODO: goBlooey

--- LunchList/src/csci498/jlamson/lunchlist/FeedActivity.java
+++ LunchList/src/csci498/jlamson/lunchlist/FeedActivity.java
@@ -1,7 +1,14 @@
 package csci498.jlamson.lunchlist;
 
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.ListActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
 public class FeedActivity extends ListActivity {
@@ -16,5 +23,40 @@ public class FeedActivity extends ListActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.feed, menu);
         return true;
+    }
+    
+    private static class FeedTask extends AsyncTask<String, Void, Void> {
+    	private Exception e;
+    	private FeedActivity activity;
+    	
+    	FeedTask(FeedActivity activity) {
+    		this.activity = activity;
+    	}
+    	
+		@Override
+		public Void doInBackground(String... urls) {
+			
+			try {
+				DefaultHttpClient client = new DefaultHttpClient();
+				HttpGet getMethod = new HttpGet(urls[0]);
+				ResponseHandler<String> responseHandler = new BasicResponseHandler();
+				String responseBody = client.execute(getMethod, responseHandler);
+				Log.d("FeedActivity", responseBody);
+			} catch (Exception e) {
+				this.e = e;
+			}
+			
+			return null;
+		}
+		
+		@Override
+		public void onPostExecute(Void unused) {
+			if (e == null) {
+				//TODO
+			} else {
+				Log.e("LunchList", "Exception parsing feed", e);
+				activity.goBlooey(e);
+			}
+		}
     }
 }
