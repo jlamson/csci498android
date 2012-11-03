500bfe0e9102f76cbd3bf302250ab68d0bf10759
Joshua Lamson <jlamson@mymail.mines.edu>
2012-11-02 23:28:28 -0600

    Created DetailActivity stub

---
+++ LunchList/src/csci498/jlamson/lunchlist/DetailForm.java
@@ -0,0 +1,20 @@
+package csci498.jlamson.lunchlist;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class DetailForm extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_form);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_detail_form, menu);
+        return true;
+    }
+}
