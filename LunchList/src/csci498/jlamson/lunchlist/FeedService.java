77c6cc1402770cc831703a0155cd1755bbb2e42f
Joshua Lamson <jlamson@mymail.mines.edu>
2012-11-03 23:41:03 -0600

    Implemented onHandleIntent with logic mirroring doInBackground().

--- LunchList/src/csci498/jlamson/lunchlist/FeedService.java
+++ LunchList/src/csci498/jlamson/lunchlist/FeedService.java
@@ -1,17 +1,29 @@
 package csci498.jlamson.lunchlist;
 
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 public class FeedService extends IntentService {
 
+	public static final String EXTRA_URL = "csci498.jlamson.lunchlist.EXTRA_URL";
+	
 	public FeedService() {
 		super("FeedService");
 	}
 
 	@Override
-	protected void onHandleIntent(Intent arg0) {
-		// TODO Auto-generated method stub
+	protected void onHandleIntent(Intent i) {
+		RSSReader reader = new RSSReader();
+		
+		try {
+			RSSFeed result = reader.load(i.getStringExtra(EXTRA_URL));
+		} catch (Exception e) {
+			Log.e("LunchListTag", "Exception parsing feed", e);
+		}
+		
 	}
 
 }
