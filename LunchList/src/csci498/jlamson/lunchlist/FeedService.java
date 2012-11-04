c596f9da1c9859b8de07c885adff09c05146eda9
Joshua Lamson <jlamson@mymail.mines.edu>
2012-11-03 23:34:18 -0600

    Created FeedService stub, and added to Manifest

---
+++ LunchList/src/csci498/jlamson/lunchlist/FeedService.java
@@ -0,0 +1,17 @@
+package csci498.jlamson.lunchlist;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class FeedService extends IntentService {
+
+	public FeedService() {
+		super("FeedService");
+	}
+
+	@Override
+	protected void onHandleIntent(Intent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+}
