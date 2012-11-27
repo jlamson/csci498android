341c45b02ffde4d180e3bcf61a2e51c07925a598
Joshua Lamson <jlamson@mymail.mines.edu>
2012-11-27 01:41:11 -0700

    created ListViewsfactory stub

---
+++ LunchList/src/jlamson/csci498/lunchlist/ListViewsFactory.java
@@ -0,0 +1,53 @@
+package jlamson.csci498.lunchlist;
+
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService.RemoteViewsFactory;
+
+public class ListViewsFactory implements RemoteViewsFactory {
+
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public RemoteViews getLoadingView() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public RemoteViews getViewAt(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getViewTypeCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean hasStableIds() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void onCreate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onDataSetChanged() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
