dd682827755a8afe1b78dde5d561c0acc87a865b
Joshua Lamson <jlamson@mymail.mines.edu>
2012-11-27 01:34:56 -0700

    implemented onHCUpdate

--- LunchList/src/jlamson/csci498/lunchlist/AppWidget.java
+++ LunchList/src/jlamson/csci498/lunchlist/AppWidget.java
@@ -1,10 +1,13 @@
 package jlamson.csci498.lunchlist;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
+import android.widget.RemoteViews;
 
 public class AppWidget extends AppWidgetProvider {
 
@@ -19,5 +22,22 @@ public class AppWidget extends AppWidgetProvider {
 
 	private void onHCUpdate(Context ctx, AppWidgetManager mgr, int[] appWidgetIds) {
 		
+		for (int i=0; i < appWidgetIds.length; i++) {
+			Intent serviceIntent = new Intent(ctx, ListWidgetService.class);
+			
+			serviceIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+			serviceIntent.setData(Uri.parse(serviceIntent.toUri(Intent.URI_INTENT_SCHEME)));
+			
+			RemoteViews widget = new RemoteViews(ctx.getPackageName(), R.layout.widget);
+			widget.setRemoteAdapter(appWidgetIds[i], R.id.restaurants, serviceIntent);
+			
+			Intent clickIntent = new Intent(ctx, DetailForm.class);
+			PendingIntent pi = PendingIntent.getActivity(context, 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+			widget.setPendingIntentTemplate(R.id.restaurants, pi);
+			
+			mgr.updateAppWidget(appWidgetIds[i], widget);
+		}
+	
+		super.onUpdate(ctx, mgr, appWidgetIds);
 	}
 }
