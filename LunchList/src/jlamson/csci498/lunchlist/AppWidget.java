a7643f0a2c31002167df9c99b25fe03e04df2467
Joshua Lamson <jlamson@mymail.mines.edu>
2012-11-27 01:23:50 -0700

    added stub for HoneyComb update in AppWidget

--- LunchList/src/jlamson/csci498/lunchlist/AppWidget.java
+++ LunchList/src/jlamson/csci498/lunchlist/AppWidget.java
@@ -4,11 +4,20 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 public class AppWidget extends AppWidgetProvider {
 
 	@Override
 	public void onUpdate(Context ctx, AppWidgetManager mgr, int[] appWidgetIds) {
-		ctx.startService(new Intent(ctx, WidgetService.class));
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			onHCUpdate(ctx, mgr, appWidgetIds);
+		} else {
+			ctx.startService(new Intent(ctx, WidgetService.class));
+		}
+	}
+
+	private void onHCUpdate(Context ctx, AppWidgetManager mgr, int[] appWidgetIds) {
+		
 	}
 }
