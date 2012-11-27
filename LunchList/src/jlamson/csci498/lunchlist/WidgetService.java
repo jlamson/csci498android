9918a2d36e626425d5c5c546c340f5c5270aac51
Joshua Lamson <jlamson@mymail.mines.edu>
2012-11-27 00:18:04 -0700

    added Widget Service

---
+++ LunchList/src/jlamson/csci498/lunchlist/WidgetService.java
@@ -0,0 +1,17 @@
+package jlamson.csci498.lunchlist;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class WidgetService extends IntentService {
+
+	public WidgetService() {
+		super("WidgetService");
+	}
+	
+	@Override
+	protected void onHandleIntent(Intent arg0) {
+
+	}
+
+}
