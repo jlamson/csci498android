870ff463b42a1995fcaf0c3ed0802eb3f441f711
Joshua Lamson <jlamson@mymail.mines.edu>
2012-11-27 03:11:40 -0700

    refactored LunchFrag to work as a fragment

--- LunchList/src/jlamson/csci498/lunchlist/LunchFragment.java
+++ LunchList/src/jlamson/csci498/lunchlist/LunchFragment.java
@@ -1,6 +1,5 @@
 package jlamson.csci498.lunchlist;
 
-import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -18,54 +17,60 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class LunchFragment extends ListActivity {
-	public final static String ID_EXTRA = "apt.tutorial._ID";
-	Cursor model = null;
-	RestaurantAdapter adapter = null;
-	RestaurantHelper helper = null;
-	SharedPreferences prefs = null;
+public class LunchFragment extends android.support.v4.app.ListFragment {
+	
+	public final static String ID_EXTRA = "jlamson.csci498.lunchlist._ID";
+	
+	Cursor model;
+	RestaurantAdapter adapter;
+	RestaurantHelper helper;
+	SharedPreferences prefs;
+	OnRestaurantListener listener;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-
-		helper = new RestaurantHelper(this);
-		prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		
+		setHasOptionsMenu(true);
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		
+		helper = new RestaurantHelper(getActivity());
+		prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
 		initList();
 		prefs.registerOnSharedPreferenceChangeListener(prefListener);
 	}
 
 	@Override
-	public void onDestroy() {
-		super.onDestroy();
-
+	public void onPause() {
 		helper.close();
+		
+		super.onPause();
 	}
 
 	@Override
 	public void onListItemClick(ListView list, View view, int position, long id) {
-		Intent i = new Intent(LunchFragment.this, DetailForm.class);
-
-		i.putExtra(ID_EXTRA, String.valueOf(id));
-		startActivity(i);
+		if (listener != null) {
+			listener.onRestaurantSelected(id);
+		}
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		new MenuInflater(this).inflate(R.menu.option, menu);
-
-		return (super.onCreateOptionsMenu(menu));
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflator) {
+		inflator.inflate(R.menu.option, menu);
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		if (item.getItemId() == R.id.add) {
-			startActivity(new Intent(LunchFragment.this, DetailForm.class));
+			startActivity(new Intent(getActivity(), DetailForm.class));
 
 			return (true);
 		} else if (item.getItemId() == R.id.prefs) {
-			startActivity(new Intent(this, EditPreferences.class));
+			startActivity(new Intent(getActivity(), EditPreferences.class));
 
 			return (true);
 		}
@@ -75,12 +80,10 @@ public class LunchFragment extends ListActivity {
 
 	private void initList() {
 		if (model != null) {
-			stopManagingCursor(model);
 			model.close();
 		}
 
 		model = helper.getAll(prefs.getString("sort_order", "name"));
-		startManagingCursor(model);
 		adapter = new RestaurantAdapter(model);
 		setListAdapter(adapter);
 	}
@@ -96,7 +99,7 @@ public class LunchFragment extends ListActivity {
 
 	class RestaurantAdapter extends CursorAdapter {
 		RestaurantAdapter(Cursor c) {
-			super(LunchFragment.this, c);
+			super(getActivity(), c);
 		}
 
 		@Override
@@ -108,7 +111,7 @@ public class LunchFragment extends ListActivity {
 
 		@Override
 		public View newView(Context ctxt, Cursor c, ViewGroup parent) {
-			LayoutInflater inflater = getLayoutInflater();
+			LayoutInflater inflater = getActivity().getLayoutInflater();
 			View row = inflater.inflate(R.layout.row, parent, false);
 			RestaurantHolder holder = new RestaurantHolder(row);
 
@@ -141,5 +144,13 @@ public class LunchFragment extends ListActivity {
 				icon.setImageResource(R.drawable.ball_green);
 			}
 		}
+	}
+	
+	public interface OnRestaurantListener {
+		void onRestaurantSelected(long id);
+	}
+	
+	public void setOnRestaurantListener(OnRestaurantListener listener) {
+		this.listener = listener;
 	}
 }
